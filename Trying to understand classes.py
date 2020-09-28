a24eccbb17793c8f74f4a86db7893a62f032226e
flower-sin <1super8cat@gmail.com>
2020-09-28 15:34:21 -0700

    owo

--- Trying to understand classes.py
+++ Trying to understand classes.py
@@ -1,1 +1,8 @@
 #hi! this is an attempt at me trying to understand classes.
+class firstclass:
+    def __init__(self,firstvalue,secondvalue):
+        self.firstvalue = firstvalue
+        self.secondvalue = secondvalue
+
+egg = firstclass("egg", True)
+
